d2e9f5f9cd9f3125214d7e0e7f031f75232b0668
Andrei Vidrighin <andrei.vidrighin@greencomputers.ro>
2014-09-16 14:54:16 +0300

    Versiunea 0

---
+++ TestGIT/Solution1.Win/WinApplication.Designer.cs
@@ -0,0 +1,67 @@
+namespace Solution1.Win
+{
+    partial class Solution1WindowsFormsApplication
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new Solution1.Module.Solution1Module();
+            this.module4 = new Solution1.Module.Win.Solution1WindowsFormsModule();
+
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = @"Integrated Security=SSPI;Pooling=false;Data Source=.\SQLEXPRESS;Initial Catalog=Solution1";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // Solution1WindowsFormsApplication
+            // 
+            this.ApplicationName = "Solution1";
+            this.Connection = this.sqlConnection1;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.Solution1WindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.Solution1WindowsFormsApplication_CustomizeLanguagesList);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private Solution1.Module.Solution1Module module3;
+        private Solution1.Module.Win.Solution1WindowsFormsModule module4;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+    }
+}
