88ae9bc39431912d49501746fb083f855fa53fd0
Andrei Vidrighin <andrei.vidrighin@greencomputers.ro>
2014-09-16 15:00:37 +0300

    Am customizat ceva

---
+++ TestGIT/Solution1.Module/BusinessObjects/ICustomEntitate.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Validation;
+
+namespace Solution1.Module.BusinessObjects
+{
+    [DomainComponent]
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[XafDefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    // Specify more UI options using a declarative approach (http://documentation.devexpress.com/#Xaf/CustomDocument2701).
+    public interface ICustomEntitate
+    {
+        //[XafDisplayName("My display name"), ToolTip("My hint message")]
+        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
+        //[RuleRequiredField(DefaultContexts.Save)]
+        //string PersistentProperty { get; set; }
+        //string CalculatedProperty { get; }
+        //int SumMethod(int val1, int val2);
+        // ...
+        // Define more data model properties and business logic contracts (http://documentation.devexpress.com/#Xaf/CustomDocument3261).
+    }
+
+    // Implement a business logic for a domain component (http://documentation.devexpress.com/#Xaf/CustomDocument3364).
+    //[DomainLogic(typeof(ICustomEntitate))]
+    //public class ICustomEntitateLogic {
+    //    public static string Get_CalculatedProperty(ICustomEntitate instance) {
+    //        // A "Get_" method is executed when getting a target property value. The target property should be readonly.
+    //        // Use this method to implement calculated properties.
+    //        return "";
+    //    }
+    //    public static void AfterChange_PersistentProperty(ICustomEntitate instance) {
+    //        // An "AfterChange_" method is executed after a target property is changed. The target property should not be readonly. 
+    //        // Use this method to refresh dependant property values.
+    //    }
+    //    public static void AfterConstruction(ICustomEntitate instance) {
+    //        // The "AfterConstruction" method is executed only once, after an object is created. 
+    //        // Use this method to initialize new objects with default property values.
+    //    }
+    //    public static int SumMethod(ICustomEntitate instance, int val1, int val2) {
+    //        // You can also define custom methods.
+    //        return val1 + val2;
+    //    }
+    //}
+
+    // Register a domain component by overriding the Setup method in a ModuleBase class descendant (http://documentation.devexpress.com/#Xaf/CustomDocument3261):
+    //public override void Setup(XafApplication application) {
+    //     XafTypesInfo.Instance.RegisterEntity("MyDomainComponent", typeof(ICustomEntitate));
+    //     base.Setup(application);
+    //}
+}
